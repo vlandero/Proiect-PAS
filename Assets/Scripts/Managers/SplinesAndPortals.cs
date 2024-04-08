6b37b9f9345626ee1f24970b416fee126b320f4b
vlandero <vlad.andrei.rosu@gmail.com>
2024-04-09 00:57:52 +0300

    add portal assets and start level selector

---
+++ Assets/Scripts/Managers/SplinesAndPortals.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class SplinesAndPortals : MonoBehaviour
+{
+    public static SplinesAndPortals Instance { get; private set; }
+    public Spline spline;
+    public GameObject spaceship;
+    [SerializeField] private GameObject portalPrefab;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        var c = GameObject.FindGameObjectsWithTag("LevelPortal").Length;
+    }
+}
